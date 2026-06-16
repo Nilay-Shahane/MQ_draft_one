35d92565c72e56ce79581104045e5d8805d6a8c5
Nilay-Shahane <nilayshahane@gmail.com>
2026-06-16 22:51:36 +0530

    new supervisor architecture basic structure

--- src/supervisor/Supervisor.js
+++ src/supervisor/Supervisor.js
@@ -1,10 +1,11 @@
 const RedisStorage = require('../infrastructure/db/RedisStorage')
-
+const JobFetcher = require('./jobfetcher/JobFetcher')
 const manager = new RedisDB(config)
 const fetcher = new RedisDB(config)
 //ignore them for now they would be in the index.js / bootstrap later
 
 class Supervisor {
+    #activeClaim = false
     constructor(name, userProcess, JobExecutor, Heartbeat, maxConcurrency = 10 ) {
         this.name = name;
         this.JobExecutor = JobExecutor;
@@ -14,35 +15,52 @@ class Supervisor {
         this.storage = new RedisStorage(this.name , manager , fetcher)
         this.activeWorkers = new Set(); 
     }
-
-    assignJob = async (jobJson) => {
-
-        const { jobId, ttl, workerId } = jobJson;
-
-        const newWorker = new this.JobExecutor(
-            this.Heartbeat, 
-            jobId, 
-            workerId, 
-            ttl, 
-            this.userProcess,
-            this.storage,
-        );
-
-        const workerPromise = newWorker.beginWork();
-
-        this.activeWorkers.add(workerPromise);
-
-
-        workerPromise.finally(() => {
-            this.activeWorkers.delete(workerPromise);
-        });
-
-        return true; 
+    hasSlot=()=>{
+        return (this.activeWorkers.size < this.maxConcurrency) ;
     }
 
-    get availableSlots() {
-        return this.maxConcurrency - this.activeWorkers.size;
+    claimHandler = () => {
+        if(this.#activeClaim) return;
+        this.#activeClaim = true;
+        while(this.hasSlot()){
+            //generate worker id 
+            //call db func
+            //if null break 
+            //if not proceed to call assign job
+            //concurrecy+=1
+        }
+        this.#activeClaim=false;
     }
+    
+
+    // assignJob = async (jobJson) => {
+
+    //     const { jobId, ttl, workerId } = jobJson;
+
+    //     const newWorker = new this.JobExecutor(
+    //         this.Heartbeat, 
+    //         jobId, 
+    //         workerId, 
+    //         ttl, 
+    //         this.userProcess,
+    //         this.storage,
+    //     );
+
+    //     const workerPromise = newWorker.beginWork();
+
+    //     this.activeWorkers.add(workerPromise);
+
+
+    //     workerPromise.finally(() => {
+    //         this.activeWorkers.delete(workerPromise);
+    //     });
+
+    //     return true; 
+    // }
+
+    // get availableSlots() {
+    //     return this.maxConcurrency - this.activeWorkers.size;
+    // }
 }
 
 module.exports = Supervisor;
